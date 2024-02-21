96f39cdccd8bd5552e52f979ef32917fbc0751ca
dmmanshin <ya.manshin@mail.ru>
2024-02-21 11:45:38 +0300

    add desktop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import {useEffect, useMemo, useRef, useState} from "react";
 import styled from "@emotion/styled";
-import bg from './assets/images/bg.png'
+import bgMobile from './assets/images/bgMobile.png'
+import bgDesktop from './assets/images/bgDesktop.png'
 import logo from './assets/images/logo.svg'
+
+const GOOGLE_TABLES_ENDPOINT_URL = 'https://script.google.com/macros/s/AKfycbwJk6keeTWnKBXtSPX1nKA2JglvHUsvxkve8Yj6y_1cgouZUrm0VE0Vh7_Jpz8v0I65/exec'
+const SPECIAL_GROUP_AMOUNT = 50
+const MIN_DESKTOP_WIDTH = 992
 
 const Wrapper = styled.div`
     position: relative;
     min-height: 100%;
-    background: url(${bg}) no-repeat 0 0 / cover;
+    background: url(${bgMobile}) no-repeat 0 0 / cover;
     padding: 105px 20px 90px;
+
+    @media (min-width: ${MIN_DESKTOP_WIDTH}px) {
+        background: url(${bgDesktop}) no-repeat 0 0 / cover;
+        padding: 220px 220px 144px;
+    }
 `
 
 const Logo = styled.img`
@@ -16,30 +26,49 @@ const Logo = styled.img`
     right: 33px;
     width: auto;
     height: 26px;
+
+    @media (min-width: ${MIN_DESKTOP_WIDTH}px) {
+        top: 62px;
+        right: 80px;
+        height: 40px;
+    }
 `
 
 const Title = styled.h2`
     font-family: 'VTB Group', sans-serif;
     font-size: 24px;
-    font-weight: 700;
     line-height: 27px;
+    font-weight: 700;
     letter-spacing: 0.01em;
     text-align: center;
     color: #1856A8;
     white-space: pre-line;
     margin: 0;
+
+    @media (min-width: ${MIN_DESKTOP_WIDTH}px) {
+        font-size: 60px;
+        line-height: 68.4px;
+    }
 `
 
 const Content = styled.div`
     display: flex;
     flex-direction: column;
     margin-top: 36px;
+
+    @media (min-width: ${MIN_DESKTOP_WIDTH}px) {
+        margin-top: 57px;
+    }
 `
 
 const Groups = styled.div`
     display: flex;
     flex-direction: column;
     margin-top: 15px;
+
+    @media (min-width: ${MIN_DESKTOP_WIDTH}px) {
+        margin-top: 26px;
+    }
 `
 
 const Group = styled.div`
@@ -49,6 +78,12 @@ const Group = styled.div`
     
     & + & {
         margin-top: 13px;
+    }
+
+    @media (min-width: ${MIN_DESKTOP_WIDTH}px) {
+        & + & {
+            margin-top: 56px;
+        }
     }
 `
 
@@ -64,16 +99,30 @@ const Item = styled.li`
     & + & {
         border: 0.3px solid #0000002E;
     }
+
+    @media (min-width: ${MIN_DESKTOP_WIDTH}px) {
+        padding: 20px 66px 22px;
+
+        & + & {
+            border: 1px solid #0000002E;
+        }
+    }
 `
 
 const Number = styled.p`
     font-family: 'ComicSansMS', 'VTB Group', sans-serif;
     font-size: 16px;
-    font-weight: 400;
     line-height: 18px;
+    font-weight: 400;
     letter-spacing: 0.01em;
     color: #000000;
     width: 42px;
+
+    @media (min-width: ${MIN_DESKTOP_WIDTH}px) {
+        font-size: 24px;
+        line-height: 27px;
+        width: 70px;
+    }
 `
 
 const Id = styled.p`
@@ -84,6 +133,12 @@ const Id = styled.p`
     letter-spacing: 0.01em;
     color: #000000;
     margin-left: 30px;
+
+    @media (min-width: ${MIN_DESKTOP_WIDTH}px) {
+        font-size: 24px;
+        line-height: 27px;
+        margin-left: 90px;
+    }
 `
 
 const Points = styled.p`
@@ -94,6 +149,11 @@ const Points = styled.p`
     letter-spacing: 0.01em;
     color: #002882;
     margin-left: auto;
+
+    @media (min-width: ${MIN_DESKTOP_WIDTH}px) {
+        font-size: 24px;
+        line-height: 27px;
+    }
 `
 
 const LoaderWrapper = styled.div`
@@ -104,6 +164,10 @@ const LoaderWrapper = styled.div`
 
 const Loader = styled.svg`
     height: 80px;
+
+    @media (min-width: ${MIN_DESKTOP_WIDTH}px) {
+        height: 160px;
+    }
 `
 
 const InputContainer = styled.div`
@@ -120,6 +184,13 @@ const InputWrapper = styled.div`
     background-color: #FFFFFF;
     border-radius: 8px;
     transition: width 200ms, padding 200ms;
+
+    @media (min-width: ${MIN_DESKTOP_WIDTH}px) {
+        width: ${({opened}) => opened ? '100%' : '74px'};
+        height: 74px;
+        border-radius: 13.5px;
+        padding: ${({opened}) => opened ? '17px 17px 17px 74px' : '17px 0 17px 74px'};
+    }
 `
 
 const Input = styled.input`
@@ -131,13 +202,18 @@ const Input = styled.input`
     border: none;
     font-family: 'ComicSansMS', 'VTB Group', sans-serif;
     font-size: 16px;
-    font-weight: 400;
     line-height: 18px;
+    font-weight: 400;
     letter-spacing: 0.01em;
     color: #000000;
     
     &::placeholder {
         color: #0000004D;
+    }
+
+    @media (min-width: ${MIN_DESKTOP_WIDTH}px) {
+        font-size: 27px;
+        line-height: 30px;
     }
 `
 
@@ -150,10 +226,13 @@ const SearchIcon = styled.svg`
     transform: translateY(-50%);
     cursor: pointer;
     z-index: 1;
-`
 
-const GOOGLE_TABLES_ENDPOINT_URL = 'https://script.google.com/macros/s/AKfycbwJk6keeTWnKBXtSPX1nKA2JglvHUsvxkve8Yj6y_1cgouZUrm0VE0Vh7_Jpz8v0I65/exec'
-const SPECIAL_GROUP_AMOUNT = 50
+    @media (min-width: ${MIN_DESKTOP_WIDTH}px) {
+        width: 40px;
+        height: 40px;
+        left: 17px;
+    }
+`
 
 function SearchInput(props) {
     const {value, placeholder, onChange} = props
